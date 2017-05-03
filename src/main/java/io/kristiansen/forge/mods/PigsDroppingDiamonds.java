eb1763f98b7d2c8fbac3dfc84efc7291c358c2ce
Morten Kristiansen <morten.kristiansen@gmail.com>
2017-05-03 19:30:24 +0100

    # Summary (50 characters or less) Missed a file in previous commit. # Why did you make this change
    # Relevant documentation
    
    # Is this related to issues / tickets ?

---
+++ src/main/java/io/kristiansen/forge/mods/PigsDroppingDiamonds.java
@@ -0,0 +1,24 @@
+package io.kristiansen.forge.mods;
+
+import java.util.Random;
+
+import net.minecraft.entity.passive.EntityPig;
+import net.minecraft.init.Items;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class PigsDroppingDiamonds {
+
+	@SubscribeEvent
+	public void dropDiamonds(LivingDeathEvent event){ 
+		if (!(event.getEntity() instanceof EntityPig)) { 
+			return;
+		}
+
+		Random random = new Random(); 
+
+		if (!event.getEntity().worldObj.isRemote) { 
+			event.getEntity().dropItem(Items.DIAMOND, 320); 
+		}
+	}
+}
